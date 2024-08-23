92cbfd808f108d079a356b9a2cef96b8f8b3d36b
Shlok Bhakta <shlokbhakta1@gmail.com>
2024-08-23 10:39:29 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Website
+
+> [!TIP] I am not using any libraries for the actual site. The reason there are vite files and stuff is so I can make development faster with hot reloading! I will remove them if neccisary but my html css and js will be vanilla with nothing else added to it 👍
---
+++ shell.nix
@@ -0,0 +1,14 @@
+{pkgs ? import <nixpkgs> {}}:
+
+pkgs.mkShell
+{
+  # https://www.youtube.com/watch?v=yQwW8dkuHqw&t=131s
+  # https://search.nixos.org/
+  # https://www.nixhub.io/
+  nativeBuildInputs = with pkgs; [
+    nodejs_22 # Pretty much only for hot reloads
+  ];
+  shellHook = ''
+    zsh
+  '';
+}
